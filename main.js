35324dd9113cb08707be396188cca7eeae463f3f
bleon133 <brot10102017@gmail.com>
2023-04-21 07:38:26 -0500

    version2

--- main.js
+++ main.js
@@ -1,42 +1,75 @@
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
+const num = document.getElementById('num1');
 const operando = document.getElementById('operando');
 const display = document.getElementById('display');
 const botones = document.querySelector('.container__parte3')
-
+let num1 = "";
 botones.addEventListener('click',(e)=>{
     switch(e.target.id){
         case 'divi':
-            operando.textContent = '/';
+            if(isNaN(num.value) || num.value == ""){
+                display.textContent = 'NaN';
+            }else{
+                operando.textContent = '/';
+                num1 = num.value;
+                num.value = "";
+            }
             break;
         case 'mult':
-            operando.textContent = '*';
+            if(isNaN(num.value) || num.value == ""){
+                display.textContent = 'NaN';
+            }else{
+                operando.textContent = '*';
+                num1 = num.value;
+                num.value = "";
+            }
             break;
         case 'rest':
-            operando.textContent = '-';
+            if(isNaN(num.value) || num.value == ""){
+                display.textContent = 'NaN';
+            }else{
+                operando.textContent = '-';
+                num1 = num.value;
+                num.value = "";
+            }
             break;  
         case 'suma':
-            operando.textContent = '+';
-            break;        
+            if(isNaN(num.value) || num.value == ""){
+                display.textContent = 'NaN';
+            }else{
+                operando.textContent = '+';
+                num1 = num.value;
+                num.value = "";
+            }
+            break;   
+        default:
+            break;
     }
     if(e.target.id=='igual'){
-        if(isNaN(num1.value) && isNaN(num2.value)){
+        if(isNaN(num.value)){
             display.textContent = 'NaN'
         }
         else{
-            if(num1.value != '' && num2.value != ''){
+            if(num.value != '' && num1 != ''){
                 switch(operando.textContent){
                     case '/':
-                        display.textContent=+num1.value / +num2.value;
+                        display.textContent=+num1 / +num.value;
+                        num.value = "";
+                        operando.textContent = '';
                         break;
                     case '*':
-                        display.textContent=+num1.value * +num2.value;
+                        display.textContent=+num1 * +num.value;
+                        num.value = "";
+                        operando.textContent = '';
                         break;
                     case '-':
-                        display.textContent=+num1.value - +num2.value;
+                        display.textContent=+num1 - +num.value;
+                        num.value = "";
+                        operando.textContent = '';
                         break;  
                     case '+':
-                        display.textContent=+num1.value + +num2.value;
+                        display.textContent=+num1 + +num.value;
+                        num.value = "";
+                        operando.textContent = '';
                         break;        
                 }
             }
@@ -44,5 +77,10 @@ botones.addEventListener('click',(e)=>{
                 display.textContent = ''
             }
         }
+    }
+    if(e.target.id=='limpiar'){
+        display.textContent= "";
+        num.value = "";
+        operando.textContent = '';
     }
 })
